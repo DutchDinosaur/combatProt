d0ffdd49a4b45db408fdff6bb329488ad8aab0a1
Dutch <carnplaynl@gmail.com>
2020-02-12 18:58:02 +0100

    kb & mouse

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,7 +2,9 @@
 
 public class InputManager : MonoBehaviour{
     public static InputManager instance;
-    
+
+    public bool Controller;
+
     public Vector2 LeftStick;
     public Vector2 RightStick;
     public bool Sprint;
@@ -17,10 +19,23 @@ public class InputManager : MonoBehaviour{
     }
 
     private void Update() {
-        LeftStick = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        RightStick = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
-        Sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) || Input.GetButton("sprint");
-        hold = Input.GetAxis("hold");
-        lower = Input.GetButton("lower") ? 1 : 0;
+        if (Controller) {
+            LeftStick = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            RightStick = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
+            Sprint = Input.GetButton("sprint");
+            hold = Input.GetAxis("hold");
+            lower = Input.GetButton("lower") ? 1 : 0;
+        }
+        else {
+            LeftStick = new Vector2(boolToFloat(Input.GetKey(KeyCode.D)) - boolToFloat(Input.GetKey(KeyCode.A)), boolToFloat(Input.GetKey(KeyCode.W)) - boolToFloat(Input.GetKey(KeyCode.S)));
+            RightStick = new Vector2(Input.mousePosition.x / Screen.width * 2 - 1, Input.mousePosition.y / Screen.height * 2 - 1);
+            Sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            hold = Input.GetKey(KeyCode.E) ? 1 : 0;
+            lower = Input.GetKey(KeyCode.Q) ? 1 : 0;
+        }
+    }
+
+    float boolToFloat(bool b) {
+        return b ? 1 : 0;
     }
 }
