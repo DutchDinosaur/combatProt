27671fabf2547331336f3af8a63555c4f740f2cf
DutchDinosaur <carnplaynl@gmail.com>
2020-03-04 22:20:32 +0100

    grabbingstuff

---
+++ Assets/Scripts/Grabing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabing : MonoBehaviour {
+
+    public FixedJoint GrabbedJoint;
+    [SerializeField] float GrabRadius;
+    [SerializeField] float holdDistance;
+    [SerializeField] float moveToHandSpeed;
+
+    CharacterController playerController;
+    Rigidbody rb;
+
+    private void Start() {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Grab() {
+        Collider[] GrabColliders = Physics.OverlapSphere(transform.position, GrabRadius, LayerMask.GetMask("grab"));
+        if (GrabColliders.Length > 0) {
+            grabObject(GrabColliders);
+        }
+    }
+
+    void grabObject(Collider[] GrabColliders) {
+        FixedJoint joint = GrabColliders[0].transform.parent.gameObject.AddComponent<FixedJoint>() as FixedJoint;
+        joint.connectedBody = rb;
+        if (joint.gameObject.tag == "Weapon") {
+            joint.transform.position = transform.position;
+            playerController.swordRB = joint.GetComponent<Rigidbody>();
+        }
+        GrabbedJoint = joint;
+    }
+
+    public void Release() {
+        GameObject.Destroy(GrabbedJoint);
+        playerController.swordRB = null;
+    }
+
+    IEnumerator MoveToGrabber() {
+        while (Vector3.Distance(GrabbedJoint.transform.position, transform.position) > holdDistance) {
+            GrabbedJoint.transform.position = Vector3.Lerp(GrabbedJoint.transform.position, transform.position, Time.deltaTime * moveToHandSpeed);
+            yield return new WaitForFixedUpdate();
+        }
+
+    }
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,28 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class InputManager : MonoBehaviour{
-    public static InputManager instance;
 
-    public bool Controller;
-
-    public Vector2 walkVector;
-    public Vector2 AimVector;
+    [SerializeField] bool Controller;
     [SerializeField] float AimPortion;
-    public bool Sprint;
-    public float hold;
-    public float lower;
-    public bool jump;
+    [SerializeField] UnityEvent grab;
+    [SerializeField] UnityEvent release;
+
+    [SerializeField] GameObject controlUI;
+
+    [HideInInspector] public static InputManager instance;
+
+    [HideInInspector] public Vector2 walkVector;
+    [HideInInspector] public Vector2 AimVector;
+
+    [HideInInspector] public bool Sprint;
+    [HideInInspector] public float hold;
+    [HideInInspector] public float lower;
+    [HideInInspector] public bool jump;
 
     void Awake() {
         if (instance != null)
             GameObject.Destroy(instance);
-        else
-            instance = this;
+        instance = this;
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2)) switchInput();
+
+        if (Input.GetKeyDown(KeyCode.E)) grab.Invoke();
+        if (Input.GetKeyDown(KeyCode.Q)) release.Invoke();
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (controlUI.active) controlUI.SetActive(false);
+            else controlUI.SetActive(true);
+        }
+
         if (Controller) {
             walkVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             AimVector = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
@@ -37,8 +52,8 @@ public class InputManager : MonoBehaviour{
             Vector2 aim = new Vector2((Input.mousePosition.x / Screen.width * 2 - 1) * (Screen.width/ Screen.height), Input.mousePosition.y / Screen.height * 2 - 1) * AimPortion;
             AimVector = (aim.magnitude < 1) ? aim : aim.normalized;
             Sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-            hold = (Input.GetKey(KeyCode.E) || Input.GetMouseButton(0)) ? 1 : 0;
-            lower = (Input.GetKey(KeyCode.Q) || Input.GetMouseButton(1)) ? 1 : 0;
+            hold = (Input.GetMouseButton(0)) ? 1 : 0;
+            lower = (Input.GetMouseButton(1)) ? 1 : 0;
             jump = Input.GetKey(KeyCode.Space);
             if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1)) SceneManager.LoadScene(0);
         }
