1e8ad92b2255977efc4be58fa1da69147beb1f89
Dutch <carnplaynl@gmail.com>
2020-03-04 22:31:12 +0100

    blockout

---
+++ Assets/Scripts/CameraControllerTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerTarget : MonoBehaviour {
+
+    public Transform trackpos;
+
+    Vector3 CamPosOffset;
+
+    Vector3 camVelocity;
+    public float smoothTime;
+
+    private void Start() {
+        CamPosOffset = transform.position;
+        //if (trackpos == null) {
+        //    trackpos = GameObject.FindGameObjectWithTag("Player").transform;
+        //}
+    }
+
+    void Update() {
+        Vector3 desiredPos = trackpos.position + CamPosOffset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPos, ref camVelocity, smoothTime);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(trackpos.position);
+    }
+}
