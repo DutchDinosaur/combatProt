cd9b5ad98b9b6aed5b36e8ba9a0b227ccdbd3f5b
DutchDinosaur <carnplaynl@gmail.com>
2020-05-17 20:20:59 +0200

    new version

---
+++ Assets/Scripts/countChildren.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countChildren : MonoBehaviour {
+    public int count;
+
+    void Start() {
+        count = transform.childCount;
+        Debug.Log(count);
+    }
+}
